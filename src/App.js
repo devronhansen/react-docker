c51efedc3036c69c8b3664968b0b8e8150a05e89
Ron <rhansen@elbgoods.de>
2018-10-21 22:29:22 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/lib/Button'
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Button bsStyle="success">Default</Button>
         </header>
       </div>
     );
